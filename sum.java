dbc8d54549efb57fe4d803205bb897f4923465f6
ishita Sarin <iishitasarin2021@gmail.com>
2022-01-27 19:35:43 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,11 @@
+import java.net.SocketImpl;
+
+public class sum {
+    public static void main(String[] args) {
+        int a =5;
+        int b =6;
+
+        System.out.println(a+b);
+    }
+    
+}
